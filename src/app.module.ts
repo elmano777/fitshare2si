3664459d552151780a96d9285af16b71db9f1264
elmano777 <rolandodiazjara@gmail.com>
2024-12-27 22:38:37 -0500

    modulos por ahora necesarios creados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { GroupsModule } from './groups/groups.module';
+import { DataModule } from './data/data.module';
+import { ProgressModule } from './progress/progress.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule, AuthModule, DashboardModule, GroupsModule, DataModule, ProgressModule],
 })
 export class AppModule { }
